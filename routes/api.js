a1ae6ac6aa41649ae97c7908fec8197e04a9e298
HarrisonPeace <69378698+HarrisonPeace@users.noreply.github.com>
2021-07-12 15:01:52 +1000

    Updated response locations for both post routes (user & course), fixed header error for put course route, updated response info to have raw data instead of edited.

--- routes/api.js
+++ routes/api.js
@@ -14,60 +14,49 @@ router.get('/', function(req, res, next) {
 
 /* GET current user */
 router.get('/users', authenticateUser, asyncHandler(async (req, res, next) => {
-  const user = await User.findByPk(req.currentUser);
-  console.log(user);
-  res.status(200).json({
-    name: `${user.firstName} ${user.lastName}`,
-    email: user.emailAddress
+  const user = await User.findByPk(req.currentUser, {
+    attributes: { exclude: ['password', 'createdAt', 'updatedAt'] } 
   });
+  res.status(200).json({ user });
 }));
 
 /* POST add new user */
 router.post('/users', asyncHandler(async (req, res, next) => {
-  let user = await User.create(req.body);
-  res.status(201).end();
+  await User.create(req.body);
+  res.location('/').status(201).end();
 }));
 
 /* GET all courses and associated user */
 router.get('/courses', asyncHandler(async (req, res, next) => {
-  const coursesInfo = await Course.findAll({
-    include: [{ model: User }], 
-    order: [[ "title", "DESC" ]] 
+  const courses = await Course.findAll({
+    include: [{ 
+      model: User,
+      attributes: { exclude: ['password', 'createdAt', 'updatedAt'] }  
+    }], 
+    order: [[ "title", "DESC" ]],
+    attributes: { exclude: ['createdAt', 'updatedAt', 'userId'] } 
   });
-  let courses = coursesInfo.map(course => {
-    return {
-      title: course.title,
-      description: course.description,
-      estimatedTime: course.estimatedTime,
-      materialsNeeded: course.materialsNeeded,
-      user: `${course.User.dataValues.firstName} ${course.User.dataValues.lastName}`,
-      userEmailAddress: course.User.dataValues.emailAddress
-    }
-  })
   res.status(200).json({ courses });
 }));
 
 /* GET specific course and associated user */
 router.get('/courses/:id', asyncHandler(async (req, res, next) => {
   const course = await Course.findByPk(req.params.id, {
-    include: [{ model: User }]
+    attributes: { exclude: ['createdAt', 'updatedAt', 'userId'] }, 
+    include: [{ 
+      model: User,
+      attributes: { exclude: ['password', 'createdAt', 'updatedAt'] }  
+    }], 
   });
   course
-  ? res.status(200).json({ 
-    title: course.title,
-    description: course.description,
-    estimatedTime: course.estimatedTime,
-    materialsNeeded: course.materialsNeeded,
-    user: `${course.User.dataValues.firstName} ${course.User.dataValues.lastName}`,
-    userEmailAddress: course.User.dataValues.emailAddress
-  })
+  ? res.status(200).json({ course })
   : res.status(404).json({ error: "No such course exists" });
 }));
 
 /* POST create a new course */
 router.post('/courses', authenticateUser, asyncHandler(async (req, res, next) => {
   let course = await Course.create(req.body);
-  res.status(201).end();
+  res.location(`/api/courses/${course.dataValues.id}`).status(201).end();
 }));
 
 /* PUT update course */
@@ -79,10 +68,10 @@ router.put('/courses/:id', authenticateUser, asyncHandler(async (req, res, next)
     if (course.User.dataValues.id === req.currentUser) {
       await course.update(req.body);
       res.status(204).end();
-c
-    } else res.status(403).json({ message: `You don't have permission to update this course` });
 
-  } else res.status(404).json({ error: "No such course exists" });
+    } else res.status(403).json({ message: `You don't have permission to update this course` }).end();
+
+  } else res.status(404).json({ error: "No such course exists" }).end();
 }));
 
 /* DELETE delete course */
